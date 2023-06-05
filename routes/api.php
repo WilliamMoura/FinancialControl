54149309edd25f1f81666197b6711e276faeb05f
Will_Moura <william@febrafar.com.br>
2023-06-05 16:29:32 -0300

    docker

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('teste', function () {
-    return 'teste';
+    return 'teste 1';
 });
 
 Route::resource('category', CategoryController::class);
